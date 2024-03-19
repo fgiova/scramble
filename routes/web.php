c08a52cc880b73412cf01057f3fcbe2cdbd48608
Michal Štván <michalstvan@gmail.com>
2024-03-19 08:46:19 +0000

    route prefix for entire group

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,10 @@
 use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(config('scramble.middleware', [RestrictedDocsAccess::class]))->group(function () {
-    Route::get(config('scramble.doc_route', 'docs').'/api.json', function (Dedoc\Scramble\Generator $generator) {
+Route::middleware(config('scramble.middleware', [RestrictedDocsAccess::class]))->prefix(config('scramble.doc_route', 'docs'))->group(function () {
+    Route::get('/api.json', function (Dedoc\Scramble\Generator $generator) {
         return $generator();
     })->name('scramble.docs.index');
 
-    Route::view(config('scramble.doc_route', 'docs').'/api', 'scramble::docs')->name('scramble.docs.api');
+    Route::view('/api', 'scramble::docs')->name('scramble.docs.api');
 });
